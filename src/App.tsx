11ec788a1cdd9e686c7098ffb8e1c76ccee6bcc5
Jowita Gogol <jmg88@bath.ac.uk>
2024-03-01 15:11:43 +0000

    Connected with API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from "react";
 import "./main.scss";
+import WeatherCard from "./components/WeatherCard";
+import { WeatherResponse } from "./types/WeatherResponse";
 
 const App = () => {
+  const [weather, setWeather] = useState<WeatherResponse | null>(null);
+
+  const getWeather = async (latitude: number, longitude: number) => {
+    const key = "61b91645fd214069b01141243240103";
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${latitude},${longitude}&aqi=no`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data: WeatherResponse = await response.json();
+      setWeather(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          getWeather(position.coords.latitude, position.coords.longitude);
+        },
+        (error) => {
+          console.error("Error getting location", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="app-container">
+      {weather ? (
+        <WeatherCard data={weather} />
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
   );
 };
 
